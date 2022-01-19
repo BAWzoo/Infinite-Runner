03dd7930ce673c23b3bc561a77b4bda8a03eb1c1
Ucef27 <pharaoh14@verizon.net>
2022-01-18 21:50:36 -0500

    Added death music

--- Sky Slider (Infinite Runner)/Assets/Scripts/GameOverScreen.cs
+++ Sky Slider (Infinite Runner)/Assets/Scripts/GameOverScreen.cs
@@ -8,13 +8,21 @@ public class GameOverScreen : MonoBehaviour
 {
 
     [SerializeField] private PlayerController player;
+
+    public AudioSource source;
+    public AudioClip clip;
     public void Setup()
     {
         player.isDead = true;
         gameObject.SetActive(true);
+
+        source.clip = clip;
+        source.volume = 0.3F;
+        source.Play();
     }
 
     public void RestartButton() {
+        source.Stop();
         SceneManager.LoadScene("Tutorial");
     }
 }
