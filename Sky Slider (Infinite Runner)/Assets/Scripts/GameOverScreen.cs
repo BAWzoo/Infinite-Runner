7739908dbe66dd77bec95f844356be8c6bed060a
g3ner1cusername <connor.l.balint@gmail.com>
2022-01-25 09:56:43 -0500

    random death message
    ^

--- Sky Slider (Infinite Runner)/Assets/Scripts/GameOverScreen.cs
+++ Sky Slider (Infinite Runner)/Assets/Scripts/GameOverScreen.cs
@@ -9,12 +9,20 @@ public class GameOverScreen : MonoBehaviour
 
     [SerializeField] private PlayerController player;
 
+    [SerializeField] private Text txt;
+
+
+    private string[] insults = new string[] { "get oofed", "oof", "finger slip?", "rip", "L", "but next time, right?", "bozo", "you suck", "nice try", "really, again?", "Death speedrun?", "Tip: you suck", "Tip: don't die" };
+
     public AudioSource source;
     public AudioClip clip;
     public void Setup()
     {
         player.isDead = true;
         gameObject.SetActive(true);
+
+        int randnum = UnityEngine.Random.Range(0, insults.Length);
+        txt.text = insults[randnum];
 
         source.clip = clip;
         source.volume = 0.3F;
